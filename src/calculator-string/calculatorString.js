721a5be35fa15c757283e1db3c4f1e2e47934d8b
Ryan <rynpowers@gmail.com>
2018-08-20 16:25:03 -0400

    added calc string fn and tests

--- src/calculator-string/calculatorString.js
+++ src/calculator-string/calculatorString.js
@@ -1,3 +1,43 @@
 const calcString = (function() {
-  return { CalculatorString: 'hi' };
+  const addDecimal = () => {
+    let hasDecimal = false;
+    return n => {
+      if (!hasDecimal) {
+        hasDecimal = true;
+        return n.length === 0 ? '0.' : `${n}.`;
+      }
+      return n;
+    };
+  };
+
+  const addComma = n => {
+    let str = '';
+    let count = 0;
+    let [num, dec] = n.split('.');
+
+    dec = dec !== undefined ? `.${dec}` : '';
+
+    for (let i = num.length - 1; i >= 0; i--) {
+      if (count && count % 3 === 0) str = `,${str}`;
+      str = num[i] + str;
+      count++;
+    }
+    return `${str}${dec}`;
+  };
+
+  const getNum = n => (isNaN(parseFloat(n)) ? 0 : parseFloat(n));
+
+  const calculatorString = () => {
+    let num = '';
+    let decimal = addDecimal();
+    return n => {
+      if (n === undefined) return getNum(num);
+      else if (n === '.') num = decimal(num);
+      else num = num + n;
+
+      return num.length === 0 ? '0' : addComma(num);
+    };
+  };
+
+  return { calculatorString };
 })();
--- src/calculator-string/calculatorString.spec.js
+++ src/calculator-string/calculatorString.spec.js
@@ -1,8 +1,97 @@
-const { CalculatorString } = calcString;
-let str;
-describe('Calculator string', () => {
-  str = CalculatorString;
-  it('should equal hi', () => {
-    expect(str).toBe('hi');
+const { calculatorString } = calcString;
+
+describe('calculator string', () => {
+  let getNum;
+  beforeEach(() => {
+    getNum = calculatorString();
+  });
+
+  it('should add number to the end of the string', () => {
+    expect(getNum('1')).toBe('1');
+    expect(getNum('2')).toBe('12');
+    expect(getNum('3')).toBe('123');
+    expect(getNum()).toBe(123);
+  });
+
+  it('should return type number when no argument is given', () => {
+    expect(getNum()).toBe(0);
+    expect(getNum('1')).toBe('1');
+    expect(getNum('2')).toBe('12');
+    expect(getNum()).toBe(12);
+  });
+
+  it('should add commas to the number', () => {
+    expect(getNum('1')).toBe('1');
+    expect(getNum('0')).toBe('10');
+    expect(getNum('0')).toBe('100');
+    expect(getNum('0')).toBe('1,000');
+    expect(getNum('0')).toBe('10,000');
+    expect(getNum('0')).toBe('100,000');
+    expect(getNum('0')).toBe('1,000,000');
+    expect(getNum()).toBe(1000000);
+  });
+  it('should add decimals', () => {
+    expect(getNum('.')).toBe('0.');
+    expect(getNum('1')).toBe('0.1');
+    expect(getNum('2')).toBe('0.12');
+    expect(getNum('3')).toBe('0.123');
+    expect(getNum()).toBe(0.123);
+
+    getNum = calculatorString();
+
+    expect(getNum('1')).toBe('1');
+    expect(getNum('2')).toBe('12');
+    expect(getNum('.')).toBe('12.');
+    expect(getNum('2')).toBe('12.2');
+    expect(getNum('3')).toBe('12.23');
+    expect(getNum()).toBe(12.23);
+  });
+  it('should add one decimal', () => {
+    expect(getNum('.')).toBe('0.');
+    expect(getNum('.')).toBe('0.');
+    expect(getNum()).toBe(0);
+
+    getNum = calculatorString();
+
+    expect(getNum('1')).toBe('1');
+    expect(getNum('2')).toBe('12');
+    expect(getNum('3')).toBe('123');
+    expect(getNum('.')).toBe('123.');
+    expect(getNum('4')).toBe('123.4');
+    expect(getNum('5')).toBe('123.45');
+    expect(getNum('.')).toBe('123.45');
+    expect(getNum('6')).toBe('123.456');
+    expect(getNum('7')).toBe('123.4567');
+    expect(getNum('8')).toBe('123.45678');
+    expect(getNum('9')).toBe('123.456789');
+    expect(getNum()).toBe(123.456789);
+  });
+  it('should add decimals and commas', () => {
+    expect(getNum('1')).toBe('1');
+    expect(getNum('0')).toBe('10');
+    expect(getNum('0')).toBe('100');
+    expect(getNum('0')).toBe('1,000');
+    expect(getNum('.')).toBe('1,000.');
+    expect(getNum('4')).toBe('1,000.4');
+    expect(getNum('5')).toBe('1,000.45');
+    expect(getNum()).toBe(1000.45);
+
+    getNum = calculatorString();
+
+    expect(getNum('1')).toBe('1');
+    expect(getNum('0')).toBe('10');
+    expect(getNum('0')).toBe('100');
+    expect(getNum('0')).toBe('1,000');
+    expect(getNum('0')).toBe('10,000');
+    expect(getNum('0')).toBe('100,000');
+    expect(getNum('0')).toBe('1,000,000');
+    expect(getNum('0')).toBe('10,000,000');
+    expect(getNum('.')).toBe('10,000,000.');
+    expect(getNum('5')).toBe('10,000,000.5');
+    expect(getNum('5')).toBe('10,000,000.55');
+    expect(getNum('5')).toBe('10,000,000.555');
+    expect(getNum('5')).toBe('10,000,000.5555');
+    expect(getNum('5')).toBe('10,000,000.55555');
+    expect(getNum()).toBe(10000000.55555);
   });
 });
