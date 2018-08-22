2911f9af05c04752b81cb8a8ea6ed0046cf96f53
Ryan <rynpowers@gmail.com>
2018-08-22 16:04:35 -0400

    added DOM object

---
+++ src/DOM/index.js
@@ -0,0 +1,29 @@
+const UI = (function() {
+  const array = s => Array.from(document.querySelectorAll(s));
+  const item = s => document.querySelector(s);
+
+  const ui = {
+    buttons: array('.btn'),
+    operators: array('.btn.btn-op'),
+    numbers: array('.btn.btn-num'),
+    execute: item('.btn.btn-execute'),
+    clear: item('.btn.btn-clear'),
+    screen: item('.screen'),
+    setScreen(val) {
+      this.screen.value = val;
+    },
+    setActiveFn(name) {
+      this.operators.forEach(item => {
+        item.value === name
+          ? item.classList.add('active')
+          : item.classList.remove('active');
+      });
+    },
+  };
+
+  ui.setScreen('screen set');
+  ui.setActiveFn('add');
+  return {
+    ui,
+  };
+})();
