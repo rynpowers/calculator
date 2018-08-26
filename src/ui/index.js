d343b6d80dc7fec605c6814bcd5ab3f0b682164b
Ryan <rynpowers@gmail.com>
2018-08-25 21:50:18 -0400

    changed ui to object constructor

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,26 +1,31 @@
-const UI = (function() {
+const uiObject = (function() {
   const array = s => Array.from(document.querySelectorAll(s));
   const elem = s => document.querySelector(s);
 
-  const ui = {
-    buttons: array('.btn'),
-    operators: array('.btn.btn-op'),
-    numbers: array('.btn.btn-num'),
-    execute: elem('.btn.btn-execute'),
-    clear: elem('.btn.btn-clear'),
-    screen: elem('.screen'),
-    setScreen(val) {
-      this.screen.value = val;
-    },
-    setActiveFn(name) {
-      this.operators.forEach(item => {
-        item.value === name
-          ? item.classList.add('active')
-          : item.classList.remove('active');
-      });
-    },
+  function UI() {
+    this.elements = {
+      buttons: array('.btn'),
+      operators: array('.btn.btn-op'),
+      numbers: array('.btn.btn-num'),
+      execute: elem('.btn.btn-execute'),
+      clear: elem('.btn.btn-clear'),
+      screen: elem('.screen'),
+    };
+  }
+
+  UI.prototype.setScreen = function(val) {
+    this.elements.screen.value = val;
   };
+
+  UI.prototype.setActiveFn = function(name) {
+    this.elements.operators.forEach(item => {
+      item.value === name
+        ? item.classList.add('active')
+        : item.classList.remove('active');
+    });
+  };
+
   return {
-    ui,
+    UI,
   };
 })();
