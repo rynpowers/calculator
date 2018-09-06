6b18f2a6957d6501c3fb16088844f1d394384522
Ryan <rynpowers@gmail.com>
2018-09-06 16:32:02 -0400

    move to es6 syntax

--- src/calculator-app/index.js
+++ src/calculator-app/index.js
@@ -1,61 +1,55 @@
-const appConnections = (function(
-  { Calculator },
-  { CalcString },
-  { CalcState },
-  { UI },
-  helpers
-) {
-  const app = function() {
-    let calc = new Calculator();
-    let calcStr = new CalcString();
-    let calcState = new CalcState();
-    let ui = new UI();
+import {
+  numberHelper,
+  operatorHelper,
+  executeHelper,
+  plusMinusHelper,
+  percentHelper,
+} from './helperFns';
+import Calculator from '../calculator-calc/Calculator';
+import CalcString from '../calculator-string/CalculatorString';
+import CalcState from '../calculator-state/CalculatorState';
+import UI from '../calculator-ui/CalculatorUi';
 
-    let setScreen = ui.setScreen.bind(ui, calcStr.numToString);
-    let setFn = calc.setFn.bind(calc);
-    let pushNum = calc.push.bind(calc);
-    let setActiveFn = ui.setActiveFn.bind(ui);
+export const app = () => {
+  let calc = new Calculator();
+  let calcStr = new CalcString();
+  let calcState = new CalcState();
+  let ui = new UI();
 
-    calcState.createState({
-      screen: '',
-      storedNum: null,
-      storedFn: '',
-      activeFn: '',
-      listener: '',
-    });
+  let setScreen = ui.setScreen.bind(ui, calcStr.numToString);
+  let setFn = calc.setFn.bind(calc);
+  let pushNum = calc.push.bind(calc);
+  let setActiveFn = ui.setActiveFn.bind(ui);
 
-    calcState.connect({
-      screen: [setScreen],
-      storedNum: [pushNum],
-      storedFn: [setFn],
-      activeFn: [setActiveFn],
-    });
+  calcState.createState({
+    screen: '',
+    storedNum: null,
+    storedFn: '',
+    activeFn: '',
+    listener: '',
+  });
 
-    calcState.setState({ screen: '0', activeFn: '' });
+  calcState.connect({
+    screen: [setScreen],
+    storedNum: [pushNum],
+    storedFn: [setFn],
+    activeFn: [setActiveFn],
+  });
 
-    const {
-      numberHelper,
-      operatorHelper,
-      executeHelper,
-      percentHelper,
-      plusMinusHelper,
-    } = helpers;
+  calcState.setState({ screen: '0', activeFn: '' });
 
-    let onClickNumber = numberHelper.bind(this, calcState, calcStr, calc);
-    let onClickOperator = operatorHelper.bind(this, calcState, calcStr, calc);
-    let onClickExecute = executeHelper.bind(this, calcState, calcStr, calc);
-    let onClickPlusMinus = plusMinusHelper.bind(this, calcState, calcStr, calc);
-    let onClickPercent = percentHelper.bind(this, calcState, calcStr, calc);
+  let onClickNumber = numberHelper.bind(this, calcState, calcStr, calc);
+  let onClickOperator = operatorHelper.bind(this, calcState, calcStr, calc);
+  let onClickExecute = executeHelper.bind(this, calcState, calcStr, calc);
+  let onClickPlusMinus = plusMinusHelper.bind(this, calcState, calcStr, calc);
+  let onClickPercent = percentHelper.bind(this, calcState, calcStr, calc);
 
-    return {
-      onClickNumber,
-      onClickOperator,
-      onClickExecute,
-      onClickPlusMinus,
-      onClickPercent,
-      ui,
-    };
+  return {
+    onClickNumber,
+    onClickOperator,
+    onClickExecute,
+    onClickPlusMinus,
+    onClickPercent,
+    ui,
   };
-
-  return app;
-})(calculator, calculatorString, calculatorState, uiObject, helpers);
+};
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { app } from './calculator-app';
+
+let {
+  onClickNumber,
+  onClickOperator,
+  onClickExecute,
+  onClickPlusMinus,
+  onClickPercent,
+  ui,
+} = app();
+
+const { numbers, operators, clear, execute, plusMinus, percent } = ui.elements;
+
+clear.addEventListener('click', () => {
+  ({
+    onClickNumber,
+    onClickOperator,
+    onClickExecute,
+    onClickPlusMinus,
+    onClickPercent,
+    ui,
+  } = app());
+});
+
+numbers.forEach(number => number.addEventListener('click', onClickNumber));
+
+operators.forEach(operator =>
+  operator.addEventListener('click', onClickOperator)
+);
+
+execute.addEventListener('click', onClickExecute);
+
+plusMinus.addEventListener('click', onClickPlusMinus);
+
+percent.addEventListener('click', onClickPercent);
